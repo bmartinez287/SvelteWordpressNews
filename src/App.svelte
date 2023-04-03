d5e78b40837a97190ea9d465b8157d313ead4472
Bernardo Martinez <bernardo-martinez@utc.edu>
2023-04-03 13:38:01 -0400

    Added utc color pallete and conditional logic to switch from one style to the next one.

--- src/App.svelte
+++ src/App.svelte
@@ -16,56 +16,63 @@
     {arrayofarticles} -->
 {/if}
 
+
+
+
+{#if window.utcitsveltewordpress.image}
+
 <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-  {#each arrayofarticles as index}
-    {#if window.utcitsveltewordpress.image} 
-    <div class="bg-white shadow hover:bg-utc-new-blue-100 border-b-4 border-utc-new-gold-500">
-      <a href={index.link} class="hover:bg-transparent text-utc-new-blue-500">
-        <div class="text-center overflow-hidden">
-          {#if index._embedded["wp:featuredmedia"] !== undefined}
-            <img class="w-full" src=" {index._embedded['wp:featuredmedia'][0].media_details.sizes
-            ['featured-blog'].source_url}"
-            alt={index.yoast_head_json.og_title}
-            />
-          {/if}
-          {#if index._embedded["wp:featuredmedia"] === undefined}
-            <img
-              class="w-full"
-              src="https://i0.wp.com/blog.utc.edu/news/files/2022/08/Founders-21-1736.jpg?zoom=2&resize=475%2C400&ssl=1"
+{#each arrayofarticles as index}
+      <div class="bg-white shadow hover:bg-utc-new-blue-100 border-b-4 border-utc-new-gold-500">
+        <a href={index.link} class="hover:bg-transparent text-utc-new-blue-500">
+          <div class="text-center overflow-hidden">
+            {#if index._embedded["wp:featuredmedia"] !== undefined}
+              <img class="w-full" src=" {index._embedded['wp:featuredmedia'][0].media_details.sizes
+              ['featured-blog'].source_url}"
               alt={index.yoast_head_json.og_title}
-            />
-          {/if}
-          <div class="px-6 py-4">
-            <div class="font-semibold text-xl text-center mb-2">
-              {@html index.title.rendered}
-            </div>
-            <!-- <p class="text-gray-700 text-base">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p> -->
-          </div>
-        </div>
-      </a>
-    </div>
-    {:else}
-        <div>
-          <!-- {index.id(index.categories[0]).name}  -->
-          <a href={index.link} class="text-utc-new-blue-500">
-            <div class="pr-4 my-4 hover:bg-utc-new-blue-100">
-            <div class="border-l-4 border-utc-new-gold-500 py-1">
-                <div class="text-base my-1 uppercase font-bold tracking-wide pl-3">
-                  {#if index._embedded['wp:term'][0][0].id === index.categories[0]}
-                  {@html index._embedded['wp:term'][0][0].name}
-                  {/if}
-                </div>
-              <div class="text-lg mb-1 pl-3">{@html index.title.rendered}</div>
+              />
+            {/if}
+            {#if index._embedded["wp:featuredmedia"] === undefined}
+              <img
+                class="w-full"
+                src="https://i0.wp.com/blog.utc.edu/news/files/2022/08/Founders-21-1736.jpg?zoom=2&resize=475%2C400&ssl=1"
+                alt={index.yoast_head_json.og_title}
+              />
+            {/if}
+            <div class="px-6 py-4">
+              <div class="font-semibold text-xl text-center mb-2">
+                {@html index.title.rendered}
               </div>
-              <!-- {# <p> {{content.news_pubdate}} </p> #} -->
             </div>
-          </a>
-        </div>
-    {/if}
-  {/each}
+          </div>
+        </a>
+      </div>
+{/each}
 </div>
+{:else}
+<div class="md:grid md:grid-cols-3 md:grid-rows-3 md:gap-2 ml-2">
+{#each arrayofarticles as index}
+<div>
+  <!-- {index.id(index.categories[0]).name}  -->
+  <a href={index.link} class="text-utc-new-blue-500">
+    <div class="pr-4 my-4 hover:bg-utc-new-blue-100">
+    <div class="border-l-4 border-utc-new-gold-500 py-1">
+        <div class="text-base my-1 uppercase font-bold tracking-wide pl-3">
+          {#if index._embedded['wp:term'][0][0].id === index.categories[0]}
+          {@html index._embedded['wp:term'][0][0].name}
+          {/if}
+        </div>
+      <div class="text-lg mb-1 pl-3">{@html index.title.rendered}</div>
+      </div>
+      <!-- {# <p> {{content.news_pubdate}} </p> #} -->
+    </div>
+  </a>
+</div>
+
+{/each}
+</div>
+{/if}
+
 
 <style>
   /* creating css loader */
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('./src/lib/colors.tailwind');
 module.exports = {
     content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
+        colors,
         extend: {},
     },
     plugins: [],
